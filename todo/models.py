8a7d3e38285e5bc11a1f6c2733cb8cfa4a294721
1F10220110 <s1f102201100@iniad.org>
2023-07-06 11:46:33 +0900

    コードを修正

--- todo/models.py
+++ todo/models.py
@@ -8,9 +8,9 @@ class Task(models.Model):
     title = models.CharField(max_length=100)
 
     completed = models.BooleanField(default=False)
-    
+
     posted_at = models.DateTimeField(default=timezone.now)
-    
+
     due_at = models.DateTimeField(null=True, blank=True)
 
     def is_overdue(self, dt):
